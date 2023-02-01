2011b3e2b4e6a2a9a15df13dd70f82753941f15a
Maria Juravic <mariajuravic@yahoo.com>
2023-02-01 11:12:37 -0800

    add user context

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import userContext from "./userContext";
+import { BrowserRouter } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
+/** Renders application */
 
 function App() {
+  const [user, setUser] = useState({
+    data: null,
+    isLoading: true
+  });
+  const [token, setToken] = useState(getTokenFromLocalStorage);
+
+  /** Set token to local storage */
+
+  function setTokenToLocalStorage(token) {
+    localStorage.setItem("token", token);
+  }
+
+  /** Get token from local storage */
+
+  function getTokenFromLocalStorage() {
+    return localStorage.getItem("token");
+  }
+
+  /** Remove token from local storage */
+
+  function removeTokenFromLocalStorage() {
+    localStorage.removeItem("token");
+  }
+
+  /** Fetches user information */
+
+  async function fetchUserDataFromAPI() {
+    const payload = decode(token);
+    const { username } = payload;
+
+    JoblyApi.token = token;
+    const currentUser = await JoblyApi.getCurrentUser(username);
+    setUser({
+      data: currentUser,
+      isLoading: false
+    });
+  }
+
+  // Every time the token changes trigger our use effect
+
+  useEffect(function fetchAndSetUserInfo() {
+    async function fetchUser() {
+
+      if (token) {
+        fetchUserDataFromAPI();
+      } else {
+        setUser({
+          data: null,
+          isLoading: false
+        });
+      }
+    }
+    fetchUser();
+  }, [token]);
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+
   );
 }
 
