aae3e8bcbde9167084981b44253b14945dbe75af
Maria Juravic <mariajuravic@yahoo.com>
2023-02-02 10:39:07 -0800

    register and signup forms

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,10 +1,71 @@
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+const inititalFormData = {
+    username: "",
+    password: ""
+};
+
 /**
- * TODO:
+ * Form for logging in.
+ *
+ *State:
+ *-formData, formError
+ *
+ * Props:
+ * -handleLogin: function to call in parent.
+ *
+ * RouteList => LoginForm
  */
-function LoginForm() {
+function LoginForm({ handleLogin }) {
+    const[formData, setFormData] = useState(inititalFormData);
+
+    const navigate = useNavigate();
+
+    /** Update form input. */
+
+    function handleChange(evt) {
+        const { name, value } = evt.target;
+        setFormData(fd => ({
+        ...fd,
+        [name]: value
+        }));
+    }
+
+    /** Call parent function and clear form. */
+
+    async function handleSubmit(evt) {
+        evt.preventDefault();
+
+        await handleLogin(formData);
+        setFormData(inititalFormData);
+        navigate("/");
+    }
 
     return (
-        <h2>LoginForm</h2>
+        <Form onSubmit={handleSubmit}>
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+                name="username"
+                value={formData.username}
+                onChange={handleChange}
+                required
+            />
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                type="password"
+                required
+            />
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form>
     )
 }
 
--- src/SignupForm.js
+++ src/SignupForm.js
@@ -1,10 +1,96 @@
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+
+const inititalFormData = {
+    username: "",
+    password: "",
+    firstName: "",
+    lastName: "",
+    email: ""
+};
+
 /**
- * TODO:
+ * Form to signup user
+ *
+ * State:
+ * -formData
+ *
+ * props:
+ * -handleRegister
+ *
+ * RouteList => SignupForm
  */
-function SignupForm() {
+function SignupForm({ handleRegister }) {
+    const [formData, setFormData] = useState(inititalFormData);
+
+    const navigate = useNavigate();
+
+    /** Update form input */
+    function handleChange(evt) {
+        const input = evt.target;
+        setFormData(formData => ({
+            ...formData,
+            [input.name]: input.value,
+        }));
+      }
+
+    /** Call parent function and clear form */
+    async function handleSubmit(evt) {
+        evt.preventDefault();
+
+        await handleRegister(formData);
+        setFormData(inititalFormData);
+        navigate("/");
+    }
+
 
     return (
-        <h2>SignupForm</h2>
+        <Form onSubmit={handleSubmit}>
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+                name="username"
+                value={formData.username}
+                onChange={handleChange}
+                required
+            />
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                type="password"
+                required
+            />
+            <Form.Label>First Name</Form.Label>
+            <Form.Control
+                name="firstName"
+                value={formData.firstName}
+                onChange={handleChange}
+                required
+            />
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control
+                name="lastName"
+                value={formData.lastName}
+                onChange={handleChange}
+                required
+            />
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                type="email"
+                required
+            />
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form>
     )
 }
 
