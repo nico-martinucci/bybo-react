a5b187a9fc0d545be1c6a6c311d67c51ed1d940e
Nico Martinucci <domenico.martinucci@gmail.com>
2023-02-25 12:29:59 -0800

    Update home page style

--- src/Home.js
+++ src/Home.js
@@ -9,11 +9,20 @@
  */
 function Home() {
 
+    const h1Style = {
+        color: "white",
+        fontSize: "1000%"
+    }
+
+    const h3Style = {
+        color: "white",
+    }
+
     return (
         <div className="bg">
             <div className="hero">
-                <h1>BYBO</h1>
-                <h3>Backyards By Owner</h3>
+                <h1 style={h1Style}>BYBO</h1>
+                <h3 style={h3Style}>Backyards By Owner</h3>
             </div>
         </div>
     )
